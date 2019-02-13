4493cabbb269cbec741adfb1f696b86ac5f8488f
Issac Teoh <42070824+bluetimer@users.noreply.github.com>
2019-02-13 17:01:56 -0500

    Created using Popcode: https://popcode.org

--- script.js
+++ script.js
@@ -6,6 +6,8 @@ var message3;
 $("button").click(function() {
     message1 = $(".response1").val();
     message2 = $(".response2").val();
+    message2 =parseInt(message2)+17;
+
     message3 = $(".response3").val();
     $(".output").append("<div class='aComment'> In 50 years,  "+ message1+"  will travel to  "+message2 + "  counties.   "+message1+ "   would travel to  "+message3 +" every year.");
 });
